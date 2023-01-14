081dcf5fb12efcc10892a0527cbeb6ac93c0b081
Alfredo <alfredofontinele08@gmail.com>
2023-01-14 14:50:27 -0300

    feat : new

--- src/middlewares/job/verifyAlreadyExistJobByName.ts
+++ src/middlewares/job/verifyAlreadyExistJobByName.ts
@@ -4,13 +4,16 @@ import { AppError } from '../../errors/appError'
 import { IJob } from './../../interfaces/job'
 
 export const verifyAlreadyExistJobByName = async (req:Request, res:Response, next:NextFunction) => {
-    const { name, level }:IJob = req.body
+    const { name, level }:IJob = await req.body
     if (!name) {
         return next()
     }
-    const existJob = await jobsRepo.findOneBy({ name: name.toLowerCase() })
-    if (existJob.name === name.toLowerCase() && existJob.level === level.toLowerCase()) {
+    const existJob = await jobsRepo.findOneBy({ 
+        name: name.toLowerCase(), level: level.toLowerCase() 
+    })
+    if (existJob) {
         throw new AppError('Job`s name and level Already Exist', 400)
     }
+    req.jobFound = existJob
     return next()
 }
