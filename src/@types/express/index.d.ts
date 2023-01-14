38cf5c14d33aae92474a260ddc4455ecec9c8fa7
Alfredo <alfredofontinele08@gmail.com>
2023-01-14 13:01:36 -0300

    teste

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,8 +1,6 @@
-import { Person } from './../../entities/Person'
+import { Person } from '../../entities/Person'
 import { Job } from "../../entities/Job"
 
-declare module 'express'
-declare module 'bcryptjs'
 declare global {
     namespace Express {
         interface Request {
