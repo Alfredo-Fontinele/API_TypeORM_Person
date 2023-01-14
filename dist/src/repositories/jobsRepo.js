7b9c954cb486483d61aafb6cea94f567daffdc41
Alfredo <alfredofontinele08@gmail.com>
2023-01-14 13:31:33 -0300

    teste novo

--- dist/src/repositories/jobsRepo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jobsRepo = void 0;
-const data_source_1 = require("../data-source");
-const Job_1 = require("../entities/Job");
-exports.jobsRepo = data_source_1.AppDataSource.getRepository(Job_1.Job);
-//# sourceMappingURL=jobsRepo.js.map
--- dist/src/repositories/personRepo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.personRepo = void 0;
-const data_source_1 = require("../data-source");
-const Person_1 = require("../entities/Person");
-exports.personRepo = data_source_1.AppDataSource.getRepository(Person_1.Person);
-//# sourceMappingURL=personRepo.js.map
