2d9d16d9e4f3d8a1816d3586d03d2575653193a4
Alfredo <alfredofontinele08@gmail.com>
2023-03-05 14:52:26 -0300

    test 2

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,8 +1,6 @@
 import { Employee } from "../../entities/Employee";
 import { Job } from "./../../entities/Job";
 
-declare module "express";
-
 declare global {
     namespace Express {
         interface Request {
