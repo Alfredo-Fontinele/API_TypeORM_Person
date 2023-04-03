d18adb0f65780dd9138b908447edd2a32a422e68
Alfredo <alfredofontinele08@gmail.com>
2023-04-03 15:39:16 -0300

    feat : deploy config

--- dist/src/repositories/employeeRepo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.employeeRepo = void 0;
-const data_source_1 = require("../data-source");
-const Employee_1 = require("../entities/Employee");
-exports.employeeRepo = data_source_1.AppDataSource.getRepository(Employee_1.Employee);
-//# sourceMappingURL=employeeRepo.js.map
