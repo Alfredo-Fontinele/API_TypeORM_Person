2301cc3450e457113f402ddda34eb2a54198666f
Alfredo <alfredofontinele08@gmail.com>
2023-04-03 11:33:07 -0300

    feat : config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,39 +1,48 @@
-import { DataSource, DataSourceOptions } from 'typeorm'
-import 'dotenv/config'
+import { DataSource, DataSourceOptions } from "typeorm";
+import path from "path";
+import "reflect-metadata";
+import "dotenv/config";
 
-const setDataSourceConfig = ():DataSourceOptions => {
-    const migrations = `${__dirname}/**/migrations/*.{ts,js}`
-    const entities = `${__dirname}/**/entities/*.{ts,js}`
-    const nodeEnv = process.env.NODE_ENV
-    if (nodeEnv === 'test') {
+const setDataSourceConfig = (): DataSourceOptions => {
+    const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+    const migrationsPath: string = path.join(
+        __dirname,
+        "./migrations/**.{js,ts}"
+    );
+    const nodeEnv = process.env.NODE_ENV;
+
+    if (nodeEnv === "production") {
         return {
-            type: 'sqlite',
-            database: ':memory:',
-            synchronize: true,
-            entities: [entities]
-        }
-    }
-    if (nodeEnv === 'production') {
-        return {
-            type: 'postgres',
+            type: "postgres",
             url: process.env.DATABASE_URL,
-            entities: [entities],
-            migrations: [migrations]
-        }
+            entities: [entitiesPath],
+            migrations: [migrationsPath],
+        };
     }
-    return {
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        synchronize: false,
-        logging: true,
-        entities: [entities],
-        migrations: [migrations]
-    }
-}
 
-const configDataSource = setDataSourceConfig()
-export const AppDataSource = new DataSource(configDataSource)
+    if (nodeEnv === "test") {
+        return {
+            type: "sqlite",
+            database: ":memory:",
+            synchronize: true,
+            entities: [entitiesPath],
+        };
+    }
+
+    return {
+        type: "postgres",
+        host: process.env.PGHOST,
+        port: parseInt(process.env.PGPORT!),
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        logging: true,
+        synchronize: false,
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+    };
+};
+
+const dataSourceConfig = setDataSourceConfig();
+
+export const AppDataSource = new DataSource(dataSourceConfig);
